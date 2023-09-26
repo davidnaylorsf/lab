4db8e40d34030bae4f8f9fbe2a788d04dd5bdd38
David Naylor <david.naylor.sf@gmail.com>
2023-09-26 23:35:26 +0000

    Extract Sections

--- jmesTest1.py
+++ jmesTest1.py
@@ -4,17 +4,28 @@ import jmespath as jp
 import json
 
 def getRecord(recordNumber, sourceData):
-    queryStr = "[0:15].Record | [?RecordNumber == `" + str(recordNumber) + "`]"
+    queryStr = "[*].Record | [?RecordNumber == `" + str(recordNumber) + "`]"
     record = jp.search(queryStr , sourceData)
     return record
 
 with open('ELEMENTS_v2.json') as f:
     data = json.load(f)
 
-record = getRecord(2, data)
+record = getRecord(18, data)
 
-idList = jp.search("[1:3]._id", data)
-recordList = jp.search("[1:3].Record", data)
+sections = jp.search("[0].Section" , record)
+references = jp.search("[0].Reference" , record)
+
+TOCHeadings = jp.search("[*].TOCHeading", sections)
+identifiers = jp.search( "[0]", sections)
+properties = jp.search( "[1]", sections)
+history = jp.search( "[2]", sections)
+description = jp.search( "[3]", sections)
+uses = jp.search( "[4]", sections)
+sources = jp.search( "[5]", sections)
+compounds = jp.search( "[6]", sections)
+isotopes = jp.search( "[6]", sections)
+
 
 
 
