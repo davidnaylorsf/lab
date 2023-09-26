edeb5ba7507a1349c88f8b804a277b8f09aca6a9
David Naylor <david.naylor.sf@gmail.com>
2023-09-26 19:38:59 +0000

    First JMESPath queries

---
+++ jmesTest1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import jmespath as jp
+import json
+
+
+with open('ELEMENTS_v2.json') as f:
+    data = json.load(f)
+
+
+result0 = jp.search("[0]", data)
+result1 = jp.search("[1:3]", data)
+result2 = jp.search("[2]", data)
+
+# ELEMENTS_v2.json
