ae7ae638622d161797f5639539b1924d8f770fa1
David Naylor <david.naylor.sf@gmail.com>
2023-09-26 22:53:11 +0000

    Cleaned

--- jmesTest1.py
+++ jmesTest1.py
@@ -11,17 +11,7 @@ def getRecord(recordNumber, sourceData):
 with open('ELEMENTS_v2.json') as f:
     data = json.load(f)
 
-result0 = jp.search("[0]", data)
-result1 = jp.search("[1:3]", data)
-recordResult1 = jp.search("[*].Record.RecordTitle" , data)
-recordResult2 = jp.search("[0:15].Record | [?RecordTitle == 'Boron'] " , data)
-
-
-recordResult3 = jp.search( queryStr1, data)
-recordResult4 = jp.search( queryStr2, data)
-# [6]['Record']['RecordTitle']
-
-record2 = getRecord(2, data)
+record = getRecord(2, data)
 
 idList = jp.search("[1:3]._id", data)
 recordList = jp.search("[1:3].Record", data)
