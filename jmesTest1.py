beb8f293212ddba1e1739493cd1ccf5394b5ce63
David Naylor <david.naylor.sf@gmail.com>
2023-09-26 22:50:57 +0000

    Construct Query String

--- jmesTest1.py
+++ jmesTest1.py
@@ -3,17 +3,28 @@ import pandas as pd
 import jmespath as jp
 import json
 
+def getRecord(recordNumber, sourceData):
+    queryStr = "[0:15].Record | [?RecordNumber == `" + str(recordNumber) + "`]"
+    record = jp.search(queryStr , sourceData)
+    return record
 
 with open('ELEMENTS_v2.json') as f:
     data = json.load(f)
 
-
 result0 = jp.search("[0]", data)
 result1 = jp.search("[1:3]", data)
+recordResult1 = jp.search("[*].Record.RecordTitle" , data)
+recordResult2 = jp.search("[0:15].Record | [?RecordTitle == 'Boron'] " , data)
+
+
+recordResult3 = jp.search( queryStr1, data)
+recordResult4 = jp.search( queryStr2, data)
+# [6]['Record']['RecordTitle']
+
+record2 = getRecord(2, data)
 
 idList = jp.search("[1:3]._id", data)
 recordList = jp.search("[1:3].Record", data)
 
-result2 = jp.search("[2]", data)
 
-# ELEMENTS_v2.json
+
