040b6eeb10dd300ec88305b3a4aa8028689b2ad4
David Naylor <david.naylor.sf@gmail.com>
2023-09-26 16:41:17 +0000

    nested dataSections

--- examine2.py
+++ examine2.py
@@ -6,17 +6,24 @@ with open('.jupyter/lab/ELEMENTS_v2.json') as f:
     data = json.load(f)
 
 records=[]
+sections=[]
+dataSections=[]
 
 for recordRow in data:
     record = recordRow['Record']
     records.append(record)
 
-sections=[]
-
 for record in records:
     section = record['Section']
     sections.append(section)
 
-
+for section in sections:
+    for sectionItem in section:
+      if hasattr(sectionItem, 'Section'):
+        dataSection=sectionItem['Section']   
+        dataSections.append(dataSection)
+      if hasattr(sectionItem, 'Information'):
+        dataSection=sectionItem['Information']   
+        dataSections.append(dataSection)        
 
 dh = data.head()
