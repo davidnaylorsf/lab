dc5e2c09db288be81a2e91bd61d9fad4234508cb
David Naylor <david.naylor.sf@gmail.com>
2023-09-29 02:01:18 +0000

    Example 2: Query Nested Fields

--- JMESPath_Exercises1.py
+++ JMESPath_Exercises1.py
@@ -8,11 +8,15 @@ import json
 with open('iTunes1.json') as f:
     data = json.load(f)
 
+# Example1
+
 query1 = "results[].{tracks:trackName,collections:collectionName}"
 query1a = "results.{tracks:trackName,collections:collectionName}"
+query1b = "results[*].{tracks:trackName,collections:collectionName}"
 
 
 example1 = jp.search(query1, data)
 example1a = jp.search(query1a, data)
+example1b = jp.search(query1b, data)
 
 print(str(example1))
