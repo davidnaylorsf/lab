62454426cc9202d669417e4ed0f5e364607aba0f
David Naylor <david.naylor.sf@gmail.com>
2023-09-29 23:44:34 +0000

    Added RecordTitle & RecordNumber

--- findingMarkup_v2.py
+++ findingMarkup_v2.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://192.168.1.239:27017/')
 db = client.Elements_PubChem
 coll = db.Markup
-updateDB = False
+updateDB = True
 
 def getRecord(recordNumber, sourceData):
     # Returns the first matching Record that matches the filter
@@ -16,7 +16,7 @@ def getRecord(recordNumber, sourceData):
 with open('ELEMENTS_v2.json') as f:
     data = json.load(f)
 
-for recordNumber in range(3,4):
+for recordNumber in range(1,21):
     
     record = getRecord(recordNumber, data)
     recType = jp.search("RecordType" , record)
@@ -27,7 +27,9 @@ for recordNumber in range(3,4):
 
     sections = jp.search("Section" , record)
     InformationQuery = "Section[].Information"
+    # InformationQuery2 = "[ RecordTitle , Section[].Information]"
     informations = jp.search(InformationQuery , record)
+    # informations2 = jp.search(InformationQuery2 , record)
     ValueQuery = "Section[].Information[].Value"
     values = jp.search(ValueQuery, record)
     SWMQuery = "Section[].Information[].Value[].StringWithMarkup"
@@ -37,5 +39,23 @@ for recordNumber in range(3,4):
     RSSI_MarkupQuery = "Section[].Section[].Information[].Value[].StringWithMarkup[].Markup"
     markups2 = jp.search(RSSI_MarkupQuery, record)
 
-    print(str(len(informations)))
+    if updateDB:
+      if markups1 is not None: 
+        if len(markups1) > 0:
+          for markup in markups1:
+             for markup_item in markup:
+                markup_item['RecordTitle']=recTitle
+                markup_item['RecordNumber']=recNumber
+                markup_id = db.Markup.insert_one(markup_item).inserted_id
+
+      if markups2 is not None: 
+        if len(markups2) > 0:
+          for markup in markups2:
+             for markup_item in markup:
+                markup_item['RecordTitle']=recTitle
+                markup_item['RecordNumber']=recNumber
+                markup_id = db.Markup.insert_one(markup_item).inserted_id
+
+
+    # print(str(len(informations)))
     # Comment
