5ba11352b378af5c889055a09e5e72fa45d36e24
David Naylor <david.naylor.sf@gmail.com>
2023-09-30 17:41:36 +0000

    Separate handling by Markup type

--- markupProc1.py
+++ markupProc1.py
@@ -6,9 +6,38 @@ client = MongoClient('mongodb://192.168.1.239:27017/')
 db = client.Elements_PubChem
 coll = db.Markup
 
-markups = coll.find({"RecordNumber": 1})
+def processMarkup(markupDocument):
+  markup = markupDocument["markup"]
+  string = markupDocument["string"]
+  stringLength = len(string)
+  for markupItem in markup:
+    start = markupItem["Start"]
+    length = markupItem["Length"]
+    if "Type" in markupItem:
+      type = markupItem["Type"]
+      extractString = string[start:start+length]
+      if type == "Italics":        
+        italicLength = length
+      elif type == "Superscript":
+        superscriptLength = length
+      elif type == "Subscript":
+        subscriptLength = length
+      elif type == "Image":
+        imageURL = markupItem["URL"]
+        imageSize = markupItem["Size"]
+        imageCaption = markupItem["Caption"]
+        imageLength = length                      
+      else:
+        print("Unhandled markup type:" + str(type))
+
+
+# markups = coll.find({"RecordNumber": 1})
+markups = coll.find({})
+retrievedCount = markups.retrieved
 
 for markupDoc in markups:
+  
+  processMarkup(markupDoc)
   markup = markupDoc["markup"]
   string = markupDoc["string"]
   
