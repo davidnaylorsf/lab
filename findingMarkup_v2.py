daf36a9a66a9c7911f8a585fb821509bb264d0a0
David Naylor <david.naylor.sf@gmail.com>
2023-09-30 15:22:41 +0000

    Markup with String and Record meta

--- findingMarkup_v2.py
+++ findingMarkup_v2.py
@@ -26,35 +26,28 @@ for recordNumber in range(1,21):
     record_meta = {'RecordType': recType, 'RecordNumber': recNumber, 'RecordTitle': recTitle}
 
     sections = jp.search("Section" , record)
-    InformationQuery = "Section[].Information"
-    # InformationQuery2 = "[ RecordTitle , Section[].Information]"
-    informations = jp.search(InformationQuery , record)
-    # informations2 = jp.search(InformationQuery2 , record)
-    ValueQuery = "Section[].Information[].Value"
-    values = jp.search(ValueQuery, record)
-    SWMQuery = "Section[].Information[].Value[].StringWithMarkup"
-    swmqs = jp.search(SWMQuery, record)
-    RSI_MarkupQuery = "Section[].Information[].Value[].StringWithMarkup[].Markup"
+
+    RSI_MarkupQuery = "Section[].Information[].Value[].StringWithMarkup[].{string:String, markup:Markup}"
     markups1 = jp.search(RSI_MarkupQuery, record)
-    RSSI_MarkupQuery = "Section[].Section[].Information[].Value[].StringWithMarkup[].Markup"
+    RSSI_MarkupQuery = "Section[].Section[].Information[].Value[].StringWithMarkup[].{string:String, markup:Markup}"
     markups2 = jp.search(RSSI_MarkupQuery, record)
 
     if updateDB:
       if markups1 is not None: 
         if len(markups1) > 0:
           for markup in markups1:
-             for markup_item in markup:
-                markup_item['RecordTitle']=recTitle
-                markup_item['RecordNumber']=recNumber
-                markup_id = db.Markup.insert_one(markup_item).inserted_id
+                if markup["markup"] is not None:
+                  markup['RecordTitle']=recTitle
+                  markup['RecordNumber']=recNumber
+                  markup = db.Markup.insert_one(markup).inserted_id
 
       if markups2 is not None: 
         if len(markups2) > 0:
           for markup in markups2:
-             for markup_item in markup:
-                markup_item['RecordTitle']=recTitle
-                markup_item['RecordNumber']=recNumber
-                markup_id = db.Markup.insert_one(markup_item).inserted_id
+                if markup["markup"] is not None:
+                  markup['RecordTitle']=recTitle
+                  markup['RecordNumber']=recNumber
+                  markup_id = db.Markup.insert_one(markup).inserted_id
 
 
     # print(str(len(informations)))
